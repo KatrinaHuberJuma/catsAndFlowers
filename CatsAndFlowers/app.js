d7e07802b698e57d70de780fadeff7ffa2034be1
KatrinaHuberJuma <katrina.huber@gmail.com>
2019-11-01 13:43:44 -0700

    express version

--- CatsAndFlowers/app.js
+++ CatsAndFlowers/app.js
@@ -1,44 +1,26 @@
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer(function (request, response){
-    console.log('client request URL: ', request.url);
-    
-    // this is how we do routing:
-    if(request.url === '/') {
-        fs.readFile('./views/index.html', 'utf8', function (errors, contents){
-            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-            response.write(contents);  //  send response body
-            response.end(); // finished!
-        });
-    }
-    else if(request.url === '/cats') {
-        fs.readFile('./views/cats.html', 'utf8', function (errors, contents){
-            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-            response.write(contents);  //  send response body
-            response.end(); // finished!
-        });
-    } else if(request.url === '/flowers') {
-        fs.readFile('./views/flowers.html', 'utf8', function (errors, contents){
-            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-            response.write(contents);  //  send response body
-            response.end(); // finished!
-        });
-    } else if(request.url.startsWith('/images')){
-        let str = request.url.substring(1);
-        console.log(str)
-        fs.readFile(str, (errors, contents) => {
-            response.writeHead(200, {'Content-type': 'image/jpg'});
-            response.write(contents); 
-            response.end();
-        });
-    }
-    // request didn't match anything:
-    else {
-        response.writeHead(404);
-        response.end('File not found!!!');
-    }
+const express = require("express");
+const app = express();
+
+
+app.use(express.static(__dirname + "/static"));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+
+
+app.get('/cats', (request, response) => {
+    response.render("cats");
 });
-// tell your server which port to run on
-server.listen(6789);
-// print to terminal window
-console.log("Running in localhost at port 6789");
+app.get('/flowers', (request, response) => {
+    response.render("flowers");
+});
+
+
+
+
+
+
+
+app.listen(8000, () => console.log("listening on port 8000"));
