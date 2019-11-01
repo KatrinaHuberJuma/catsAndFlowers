efc6b4bc80c9aa4fb8dd635a549cfea0dace1878
KatrinaHuberJuma <katrina.huber@gmail.com>
2019-11-01 15:49:55 -0700

    complete

--- CatsAndFlowers/app.js
+++ CatsAndFlowers/app.js
@@ -7,7 +7,12 @@ app.use(express.static(__dirname + "/static"));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-
+var catsObj = {
+    "cuddles": {"name":"Cuddles", "hobbies":["cuddles", "snuggles", "birdwatching"], "napspot":"Any lap!"},
+    "yarn": {"name":"Liam", "hobbies":["multi-threaded play", "knitting", "bungee jumping"], "napspot":"N/A, Liam doesn't nap"},
+    "ocelot": {"name":"Hunter", "hobbies":["animal rights activism", "raising awareness"], "napspot":"Any sofa that will have him"},
+    "kitten": {"name":"Lucy", "hobbies":["adventuring", "misc mischeif", "painting"], "napspot":"Under the Stars"}
+    }
 
 
 app.get('/cats', (request, response) => {
@@ -19,7 +24,13 @@ app.get('/flowers', (request, response) => {
 app.get('/form', (request, response) => {
     response.render("form");
 });
-
+app.get('/cats', (request, response) => {
+    response.render("cats");
+});
+app.get('/cats/:catname/', function (request, response) {
+    var cat = catsObj[request.params.catname]
+    response.render("cat", {cat:cat})
+})
 
 
 
